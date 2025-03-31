5ff18aefd5ac9c80124bcb948a09d228bdd6e2d6
hibran <hibranacosta5@gmail.com>
2025-03-30 19:09:31 -0600

    feat:implemented load pixerls for the capture , and make an range to detect certeain range of rgb , i will use this to teach my AI , welcome to the world of the of vision

--- sketch.js
+++ sketch.js
@@ -1,29 +1,47 @@
-let pixelsRGB;
-let pixels = [] , cols , rows, size = 5;
+let capture;
+let cols, rows, size = 1;
+
 function preload() {
   capture = createCapture(VIDEO);
 }
+
 function setup() {
   createCanvas(400, 400);
   capture.size(400, 400);
   capture.hide();
-  cols = width/size;
-  rows = height/size;
+  cols = width / size;
+  rows = height / size;
   noStroke();
-
 }
+
 function draw() {
   background(220);
-  for (let x = 0; x < cols; x++) {
-    pixels[x] = []
-  for (let y = 0; y < rows; y++) {
-    let i = x*size;
-    let j = y * size;
-    pixels[x][y] = capture.get(i,j);
-    fill(pixels[x][y]);
-    rect(i,j,size,size);
+  capture.loadPixels();
+
+  if (capture.pixels.length > 0) {
+    for (let x = 0; x < cols; x++) {
+      for (let y = 0; y < rows; y++) {
+        let i = x * size;
+        let j = y * size;
+        let index = (j * capture.width + i) * 4;
+        let r = capture.pixels[index];
+        let g = capture.pixels[index + 1];
+        let b = capture.pixels[index + 2];
+        if(verifyColor(r,g,b)){
+          fill([0,0,0]);
+        }else{
+          fill(r,g,b)
+        }
+        rect(i, j, size, size);
+      }
+    }
   }
-  
 }
- frameRate(10) 
+
+function verifyColor(r, g, b) {
+  return (
+    r >= 142 && r <= 255 &&
+    g >= 125 && g <= 255 &&
+    b >= 53 && b <= 255
+  );
 }
